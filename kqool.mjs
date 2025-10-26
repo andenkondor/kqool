90c0338d9709e1a4d0720366d22cc24d3f655099
Timo Rolfsmeier <timo.rolfsmeier@device-insight.com>
2025-10-26 16:00:39 +0100

    chore: make code cleaner

--- kqool.mjs
+++ kqool.mjs
@@ -2,33 +2,31 @@
 
 $.verbose = false;
 
-const KQOOL_EXECUTABLE = "kqool";
+const KQOOL_EXECUTABLE = "./kqool.mjs";
 const CONFIG_FILE = `${os.homedir()}/.kqool.yaml`;
-// const CONFIG_FILE = `.kqool.example.yaml`;
 const HISTORY_FILE = `${os.homedir()}/.kqool.history.kql`;
 
-async function checkForDependencies() {
-  const hasJq = await which("jq", { nothrow: true });
-  if (!hasJq) {
-    echo(chalk.red("Please install jq to use kqool"));
-    process.exit();
-  }
-  const hasFzf = await which("fzf", { nothrow: true });
-  if (!hasFzf) {
-    echo(chalk.red("Please install fzf to use kqool"));
-    process.exit();
-  }
-}
+const NTH = {
+  QUERY_LINE: "1",
+  QUERY_OBJECT: "2",
+};
 
 async function saveAsHistory(fileContent) {
   if (!fs.existsSync(HISTORY_FILE)) {
-    echo(chalk.red("Query is not saved to history."));
     echo(
-      chalk.red(`Create file ${HISTORY_FILE} to automatically save queries `),
+      chalk.red(
+        `Query is not saved to history.
+Create file ${HISTORY_FILE} to automatically save queries `,
+      ),
     );
     return;
   }
-  fs.appendFileSync(HISTORY_FILE, `\n${fileContent}`);
+  fs.appendFileSync(
+    HISTORY_FILE,
+    `
+// ${new Date().toISOString()}
+${fileContent}`,
+  );
 }
 
 function getConfig() {
@@ -55,13 +53,14 @@ function toBase64(input) {
 }
 
 const HELP_TEXT = [
-  chalk.italic(chalk.red("Help")),
+  chalk.italic.red("Help"),
   "Type in any text to narrow the search.",
   "You're current query is shown in the right panel.",
   "<Enter>: Add current selection to your overall query. Resets current search text.",
   "<Ctrl-d>: Same as <Enter>, but preserves search",
   "<Ctrl-u>: Undo last selection.",
   "<Ctrl-c>: Exit kqool and print overall query",
+  "<Ctrl-w>: Show jump labels",
   "<F1>: Show help section",
   "<Esc>: Hide help section",
 ].join("\n");
@@ -69,21 +68,21 @@ const HELP_TEXT = [
 const currentTmpFiles = [];
 
 function createTempFile(content) {
-  const filePath = tmpfile("kqlinator.kql", content);
+  const filePath = tmpfile("kqool", JSON.stringify(content));
   currentTmpFiles.push(filePath);
   return filePath;
 }
 
-function replacePlaceholders(str, placeholderValues) {
-  if (!str) {
+function replacePlaceholders(fragment, placeholderValues) {
+  if (!fragment) {
     return [];
   }
   const regex = /{{(.*?)}}/g;
-  const matches = [...str.matchAll(regex)];
+  const matches = [...fragment.matchAll(regex)];
 
-  if (matches.length === 0) return [str];
+  if (matches.length === 0) return [fragment];
 
-  let combinations = [str];
+  let combinations = [fragment];
 
   for (const match of matches) {
     const key = match[1];
@@ -97,28 +96,28 @@ function replacePlaceholders(str, placeholderValues) {
   return combinations;
 }
 
-async function reload(internalQueryFile) {
-  const config = getConfig();
-  const currentQueryObjects = (await $`cat ${internalQueryFile}`.lines()).map(
-    (q) => JSON.parse(q),
+async function reload(stateFile) {
+  const { config, selection } = JSON.parse(
+    fs.readFileSync(stateFile, {
+      encoding: "utf8",
+      flag: "r",
+    }),
   );
 
-  const context = config.defaultPlaceholderTransformation ?? {};
+  const placeHolderTransformations =
+    config.defaultPlaceholderTransformation ?? {};
 
-  currentQueryObjects
+  selection
     .map((q) => q.placeholderTransformation)
     .filter((p) => Boolean(p))
     .forEach((placeholderTransformation) => {
       Object.keys(placeholderTransformation).forEach((placeholderKey) => {
-        if (context[placeholderKey]) {
-          context[placeholderKey].push(
-            ...placeholderTransformation[placeholderKey].filter(
-              (p) => !context[placeholderKey].includes(p),
-            ),
-          );
-        } else {
-          context[placeholderKey] = placeholderTransformation[placeholderKey];
-        }
+        placeHolderTransformations[placeholderKey] = [
+          ...new Set(
+            ...(placeHolderTransformations[placeholderKey] ?? []),
+            placeholderTransformation[placeholderKey],
+          ),
+        ];
       });
     });
 
@@ -126,7 +125,7 @@ async function reload(internalQueryFile) {
     .flatMap((fragment) => {
       const queriesWithReplacement = replacePlaceholders(
         fragment.query,
-        context,
+        placeHolderTransformations,
       );
 
       return queriesWithReplacement.map(
@@ -139,23 +138,33 @@ async function reload(internalQueryFile) {
   return;
 }
 
-async function handleAddSelection(selection, internalQueryFile) {
-  const addSelectionObject = await $`echo ${selection} | base64 -d`.text();
-  await fs.appendFile(internalQueryFile, addSelectionObject + "\n");
-  await reload(internalQueryFile);
+async function handleAddSelection(encodedSelection, stateFile) {
+  const decodedSelection = await $`echo ${encodedSelection} | base64 -d`.json();
+  const state = JSON.parse(
+    fs.readFileSync(stateFile, {
+      encoding: "utf8",
+      flag: "r",
+    }),
+  );
+
+  const newState = {
+    ...state,
+    selection: [...state.selection, decodedSelection],
+  };
+  fs.writeFileSync(stateFile, JSON.stringify(newState));
+  await reload(stateFile);
 }
 
 async function main() {
-  await checkForDependencies();
   const {
     s: internalAddSelection,
-    f: internalQueryFile,
+    f: internalStateFile,
     r: internalReload,
     h: showHelp,
   } = minimist(process.argv.slice(3), {
     alias: {
       s: "internal-add-selection",
-      f: "internal-query-file",
+      f: "internal-state-file",
       r: "internal-reload",
       h: "help",
     },
@@ -167,79 +176,92 @@ async function main() {
     return;
   }
 
-  if (internalAddSelection && internalQueryFile) {
-    await handleAddSelection(internalAddSelection, internalQueryFile);
-    return;
-  }
-  if (internalReload && internalQueryFile) {
-    await reload(internalQueryFile);
+  if (internalAddSelection && internalStateFile) {
+    await handleAddSelection(internalAddSelection, internalStateFile);
     return;
   }
 
-  const queryFile = await createTempFile();
+  if (internalReload && internalStateFile) {
+    await reload(internalStateFile);
+    return;
+  }
+
+  const stateFile = await createTempFile({
+    config: getConfig(),
+    selection: [],
+  });
+
+  echo(stateFile);
 
   $.spawnSync(
     "fzf",
     [
+      // flags
       "--border",
-      "--read0",
       "--gap",
       "--highlight-line",
-      ...["--delimiter", "\t"],
-      ...["--with-nth", "1"],
-      ...["--preview", `jq -r '.query' ${queryFile}`],
-      ...[
-        "--bind",
-        `start:transform(${KQOOL_EXECUTABLE} --internal-reload --internal-query-file ${queryFile})`,
-      ],
-      ...[
-        "--bind",
-        `enter:transform(${KQOOL_EXECUTABLE} --internal-add-selection={2} --internal-query-file ${queryFile})+change-query()`,
-      ],
-      ...[
-        "--bind",
-        `ctrl-d:transform(${KQOOL_EXECUTABLE} --internal-add-selection={2} --internal-query-file ${queryFile})`,
-      ],
-      ...[
-        "--bind",
-        `ctrl-u:execute-silent(sed -i "" -e "$ d" ${queryFile})+refresh-preview+transform(${KQOOL_EXECUTABLE} --internal-reload --internal-query-file ${queryFile})`,
-      ],
-      ...["--bind", `ctrl-c:abort`],
-      ...["--bind", `f1:change-footer(${HELP_TEXT})`],
-      ...["--bind", `esc:change-footer()`],
-      ...[
-        "--bind",
-        `ctrl-w:jump,jump:transform(${KQOOL_EXECUTABLE} --internal-add-selection={2} --internal-query-file ${queryFile})+change-query()`,
-      ],
+      "--read0",
+      // simple
       ...["--border", "rounded"],
-      ...["--color", "border:#3344cb,label:#aeaeae,query:#d9d9d9"],
-      ...[
-        "--color",
-        "fg:#d0d0d0,fg+:#d0d0d0,bg:#121212,bg+:#7f1313,gutter:#121212",
-      ],
-      ...["--color", "hl:#5f87af,hl+:#5fd7ff,info:#afaf87,marker:#87ff00"],
-      ...[
-        "--color",
-        "prompt:#d7005f,spinner:#af5fff,pointer:#af5fff,header:#87afaf",
-      ],
-      ...[
-        "--jump-labels",
-        "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-      ],
+      ...["--delimiter", "\t"],
       ...["--marker", ">"],
       ...["--pointer", "◆"],
+      ...["--preview", `jq -r '.selection[].query' ${stateFile}`],
       ...["--preview-window", "border-rounded"],
       ...["--prompt", "> "],
       ...["--reverse"],
       ...["--scrollbar", "│"],
       ...["--separator", "─"],
+      ...["--with-nth", NTH.QUERY_LINE],
+      ...[
+        "--jump-labels",
+        "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      ],
+      // bindings
+      ...[
+        `ctrl-c:abort`,
+        `ctrl-d:transform(${KQOOL_EXECUTABLE} --internal-add-selection={${NTH.QUERY_OBJECT}} --internal-state-file ${stateFile})`,
+        `ctrl-u:execute-silent(sed -i "" -e "$ d" ${stateFile})+refresh-preview+transform(${KQOOL_EXECUTABLE} --internal-reload --internal-state-file ${stateFile})`,
+        "ctrl-w:" +
+          [
+            `jump,jump:transform(${KQOOL_EXECUTABLE} --internal-add-selection={${NTH.QUERY_OBJECT}} --internal-state-file ${stateFile})`,
+            "change-query()",
+          ].join("+"),
+        "enter:" +
+          [
+            `transform(${KQOOL_EXECUTABLE} --internal-add-selection={${NTH.QUERY_OBJECT}} --internal-state-file ${stateFile})`,
+            "change-query()",
+          ].join("+"),
+        `esc:change-footer()`,
+        `f1:change-footer(${HELP_TEXT})`,
+        `start:transform(${KQOOL_EXECUTABLE} --internal-reload --internal-state-file ${stateFile})`,
+      ].flatMap((s) => ["--bind", s]),
+      // colors
+      ...[
+        "bg+:#7f1313",
+        "bg:#121212",
+        "border:#3344cb",
+        "fg+:#d0d0d0",
+        "fg:#d0d0d0",
+        "gutter:#121212",
+        "header:#87afaf",
+        "hl+:#5fd7ff",
+        "hl:#5f87af",
+        "info:#afaf87",
+        "label:#aeaeae",
+        "marker:#87ff00",
+        "pointer:#af5fff",
+        "prompt:#d7005f",
+        "query:#d9d9d9",
+        "spinner:#af5fff",
+      ].flatMap((c) => ["--color", c]),
     ],
     {
       encoding: "utf-8",
     },
   );
 
-  const finalQuery = await $`jq -r '.query' ${queryFile}`.text();
+  const finalQuery = await $`jq -r '.selection[].query' ${stateFile}`.text();
 
   if (!finalQuery) {
     echo("no query captured");
